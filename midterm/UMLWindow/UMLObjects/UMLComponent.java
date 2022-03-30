62fad9c1e58ce922d90a14b75a482ee1057d31ea
Ping <44315881+Ping6666@users.noreply.github.com>
2022-03-30 16:17:39 +0800

    Delete UMLComponent.java

--- midterm/UMLWindow/UMLObjects/UMLComponent.java
+++
@@ -1,9 +0,0 @@
-package UMLWindow.UMLObjects;
-
-public class UMLComponent {
-    public enum SelectStatus {
-        ss_select,
-        ss_associationline, ss_generalizationline, ss_compositionline,
-        ss_class, ss_usecase,
-    };
-}
